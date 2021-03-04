a8c1bf56bba362f014738c2a662f62d2e8c1b139
hp0404 <hryhorii.pavlenko@gmail.com>
2021-03-04 10:13:44 +0200

    setup related stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    author="HTU",
+    author_email="hcssteamukraine@gmail.com",
+    name="corpus",
+    version="0.1",
+    description="base corpora stats",
+    packages=find_packages()
+)
